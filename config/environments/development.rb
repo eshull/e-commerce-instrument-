e240833d95c54bf2332c52f544db44c0c5b50fff
Eric Shull <eshulldesign@gmail.com>
2018-10-01 11:31:34 -0700

    add most of configuration for email password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
