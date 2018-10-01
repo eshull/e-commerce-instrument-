d42976f3674059f2d7bb770d33b420d6d49cdd1c
Eric Shull <eshulldesign@gmail.com>
2018-10-01 11:41:19 -0700

    add feature for email generation on sign up

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'test@example.com'
+
+  def send_welcome_email(user)
+    @user = user
+    mail(:to => @user.email, :subject => "Welcome!")
+  end
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,13 +31,26 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  config.action_mailer.perform_deliveries = true
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
