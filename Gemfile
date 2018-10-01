dc61ac8904f4c0344d65a2fb9cebb1fa5aeedd62
Eric Shull <eshulldesign@gmail.com>
2018-10-01 10:12:20 -0700

    add rails admin gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.2.2'
 gem 'faker'
 gem 'jquery-rails'
 gem 'devise'
+gem 'rails_admin', '~> 1.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # devise_for :users
+devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      passwords: 'users/passwords',
+      registrations: 'users/registrations'
+  }
   root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
