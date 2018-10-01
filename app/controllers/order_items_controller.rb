079c5e587ad9c2cb1ec14f43a20778ac5ae88b06
Eric Shull <eshulldesign@gmail.com>
2018-10-01 13:35:18 -0700

    add views, models, controller for order items and cart

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,30 @@
+class OrderItemsController < ApplicationController
+
+  def create
+    @order = current_order
+    @item = @order.order_items.new(item_params)
+    @order.save
+    session[:order_id] = @order.id
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
+    end
+  end
+
+  def destroy
+    @order = current_order
+    @item = @order.order_items.find(params[:id])
+    @item.destroy
+    @order.save
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.js
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:order_item).permit(:quantity, :product_id)
+  end
+end
