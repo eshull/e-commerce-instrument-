864b42592e86c7141fab20383505953aec63eb41
Eric Shull <eshulldesign@gmail.com>
2018-10-03 10:39:17 -0700

    add header and image to products

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :order_items
   resources :products
   resource :cart, only: [:show]
+  resource :user, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
